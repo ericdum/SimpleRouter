b521787dbe5c7d05a608f3bba230b659479c8a4a
eric <formateric@gmail.com>
2013-06-18 16:18:11 +0800

    第一稿

--- README.md
+++ README.md
@@ -2,3 +2,63 @@ SimpleRouter
 ============
 
 一个简单的router，根据#!action/param1/param2....:key1=value:key2=value....这样的链接交给controller处理来无刷跳转页面。
+
+##鸣谢
+感谢[jQuery hashchange](https://github.com/cowboy/jquery-hashchange)项目让某些浏览器支持了onhashchange事件
+
+##为什么做这个
+
+现在经常遇到做一个小站同时页面跳转要是通过js控制的，并且还要支持通过url直接访问二级页面如：domain.com/#!about-me要直接访问到about-me页面。
+
+以前每个项目都单独写个简单的路由，这次又遇到这个问题，干脆就写一个简单、好扩展的，以后直接引入就行了。
+
+##依赖
+1. jquery
+2. hashchange(直接包入了本项目js) - 浏览器兼容方案
+
+##使用方法
+1. 引入simple-router.js
+2. 编写controller及其他特殊路由规则router.rules
+3. 使用`<a href="#!action/...">action</a>`
+4. 空hash调用controller.index
+
+##window.router
+----
+###router.rules
+是一系列action和处理函数为键值对的对象。处理函数可以为函数也可以是字符串，如果是字符串，router将到controller里面去查找同名的函数。
+
+	router.rules = {
+		page1 : function(){ 
+			//url#!page1 
+		},
+		page2 : 'back'
+	}
+	controller.back = function(){
+		//url#!page2
+	}
+	
+###router.action
+当前action的处理函数
+
+##window.request
+----
+###request.hash
+完整的hash字符串（删除了#!）
+###request.action
+当前的action名称，如果不存在，则为index
+###request.params
+当前请求的参数列表（被依次传入action）
+###request.querys
+类似于http get请求的get数组
+
+##window.controller
+----
+
+###controller.index
+根action
+###controller.noFound
+未找到任何匹配的action时调用
+###controller.beforeFilter
+调用任何action之前调用
+###controller.afterFilter
+调用任何action之后调用
---
+++ simple-router.js
@@ -0,0 +1,119 @@
+/**
+ * Simple Router
+ *
+ * Copyright 2013, Eric Dum 代立晨
+ * http://niuble.com
+ *
+ * Free to use and abuse under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ */
+
+/*
+ * jQuery hashchange event - v1.3 - 7/21/2010
+ * http://benalman.com/projects/jquery-hashchange-plugin/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+
+(function($,e,b){
+    var c="hashchange",h=document,f,g=$.event.special,i=h.documentMode,d="on"+c in e&&(i===b||i>7);function a(j){j=j||location.href;return"#"+j.replace(/^[^#]*#?(.*)$/,"$1")}$.fn[c]=function(j){return j?this.bind(c,j):this.trigger(c)};$.fn[c].delay=50;g[c]=$.extend(g[c],{setup:function(){if(d){return false}$(f.start)},teardown:function(){if(d){return false}$(f.stop)}});f=(function(){var j={},p,m=a(),k=function(q){return q},l=k,o=k;j.start=function(){p||n()};j.stop=function(){p&&clearTimeout(p);p=b};function n(){var r=a(),q=o(m);if(r!==m){l(m=r,q);$(e).trigger(c)}else{if(q!==m){location.href=location.href.replace(/#.*/,"")+q}}p=setTimeout(n,$.fn[c].delay)}$.browser.msie&&!d&&(function(){var q,r;j.start=function(){if(!q){r=$.fn[c].src;r=r&&r+a();q=$('<iframe tabindex="-1" title="empty"/>').hide().one("load",function(){r||l(a());n()}).attr("src",r||"javascript:0").insertAfter("body")[0].contentWindow;h.onpropertychange=function(){try{if(event.propertyName==="title"){q.document.title=h.title}}catch(s){}}}};j.stop=k;o=function(){return a(q.location.href)};l=function(v,s){var u=q.document,t=$.fn[c].domain;if(v!==s){u.title=h.title;u.open();t&&u.write('<script>document.domain="'+t+'"<\/script>');u.close();q.location.hash=v}}})();return j})()})(jQuery,this);
+
+
+/**
+ *
+ * 定义了三个全局变量：controller, request, router
+ *
+ * Usage: #!action/params1/params2/params3...:key=value:key2=value...
+ *
+ */
+(function(){
+
+    var controller = {
+        index: function(){},
+        beforeFilter: function(){},
+        afterFilter: function(){},
+        noFound: function(){}
+    };
+    
+    var request = {
+        hash   : "",
+        action : "", //action名
+        params : [],
+        querys : {},
+        reset: function(){
+            this.hash   = "";
+            this.action = "";
+            this.params = [];
+            this.querys = {};
+        },
+        init: function( hash ){
+            this.reset();
+            this.hash = hash.replace('#!', '');
+            var parsed = this.hash.match(/^\/?([^:]*)?(:.+)?$/i);
+            if(parsed[1]){
+                this.params = parsed[1].split('/');
+                this.action = this.params.shift();
+            }else{
+                this.action = 'index';
+            }
+            if(parsed[2]){
+                $.each(parsed[2].split(':'), function(i,v){ 
+                    v = v.split('=');
+                    if(v[0]){
+                        request.querys[v[0]] = v[1];
+                    }
+                })
+            }
+        }
+    };
+
+    var router = (function() {
+
+        return {
+            action: function(){}, //action函数
+
+            rules: {},
+
+            open : function( hash ){
+                request.init(hash);
+
+                if ( typeof this.rules[request.action] === "function" ) {
+                    this.action = this.rules[request.action];
+
+                } else if ( typeof this.rules[request.action] == "string" &&
+                    typeof controller[this.rules[request.action]] === "function" ) {
+                    this.action = controller[this.rules[request.action]];
+
+                } else if ( typeof controller[request.action] === "function" ){
+                    this.action = controller[request.action];
+
+                } else {
+                    this.action = controller.noFound;
+                }
+                this.do_action();
+            },
+
+            do_action: function (){
+                controller.beforeFilter(request.action, request.params);
+                this.action.apply(this, request.params);
+                controller.afterFilter(request.action, request.params);
+            }
+        };
+        
+    })();
+
+
+    $(window).hashchange(function(event){
+        router.open(location.hash);
+    });
+
+    $(function(){$(window).hashchange()});
+
+    window.router = router;
+    window.request = request;
+    window.controller = controller;
+})();
+
